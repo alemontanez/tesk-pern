37795cd12ab9358a5f54e7d8640e0a68389162d3
alemontanez <alemontanez99@gmail.com>
2025-02-16 17:07:23 -0300

    Login y logout implementados

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 // Controlador que llama al servicio de autenticación y lo ejecuta.
 // Controla respuestas http al cliente.
-import { registerUser } from '../services/auth.service.js'
+import { loginUser, registerUser } from '../services/auth.service.js'
 import { createAccessToken } from '../utils/jwt.js'
 
 export const register = async (req, res) => {
@@ -30,5 +30,22 @@ export const register = async (req, res) => {
 }
 
 export const login = async (req, res) => {
+  const { email, password } = req.body
+  try {
+    const user = await loginUser({ email, password })
+    const token = await createAccessToken({ id: user.id })
+    res.cookie('token', token)
+    res.status(200).json({ message: `User ${user.username} logged successfully` })
+  } catch (error) {
+    console.log(error)
+    if (error.message === 'Invalid credentials') {
+      return res.status(400).json({ message: error.message })
+    }
+    return res.status(500).json({ message: 'Internal error' })
+  }
+}
 
+export const logout = (req, res) => {
+  res.clearCookie('token')
+  return res.sendStatus(200)
 }
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -6,7 +6,7 @@ import bcrypt from 'bcryptjs'
 export const registerUser = async ({ username, email, first_name, last_name, password }) => {
   const password_hash = await bcrypt.hash(password, 10)
 
-  // Hacer validación de username y email disponible
+  // Validación de username y email disponible
   const existingUsername = await User.findOne({ where: { username: username } })
   if (existingUsername) throw new Error('Username already exists')
 
@@ -20,5 +20,16 @@ export const registerUser = async ({ username, email, first_name, last_name, pas
     last_name,
     password_hash
   })
-return user
+  return user
+}
+
+// Login del usuario
+export const loginUser = async ({ email, password }) => {
+  const existingUser = await User.findOne({ where: { email: email } })
+  if (!existingUser) throw new Error('Invalid credentials')
+
+  const isPasswordValid = await bcrypt.compare(password, existingUser.password_hash)
+  if (!isPasswordValid) throw new Error('Invalid credentials')
+
+  return existingUser
 }
